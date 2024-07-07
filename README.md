df0329995b027a51fd996477a3668811e665693e
Cedric <mcdougal.ce@gmail.com>
2024-07-07 11:15:34 -0400

    Fix bug where feed images would not render on page load

--- README.md
+++ README.md
@@ -2,13 +2,10 @@
 
 ## Todo
 
-- Upload low res version of image to Supabase storage
-- Display low res version of image on the feed
 - Support re-ordering of posts on feed
 - Show controls when clicking a post on feed: remove, edit
 - Show reel icon on reels on feed
 - Support carousel in AddPlannedPostsDialog
-- Fix bug where images sometimes aren't loaded on the feed
 
 ## Getting Started
 
--- packages/app/src/pageComponents/FeedPage/FeedGrid/ActualFeedItem/ActualFeedItem.tsx
+++ packages/app/src/pageComponents/FeedPage/FeedGrid/ActualFeedItem/ActualFeedItem.tsx
@@ -1,20 +1,32 @@
+'use client';
+
 import { InstagramMediaItem } from '@/server/instagram';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 type Props = {
   actualPost: InstagramMediaItem;
 };
 
 const ActualFeedItem = ({ actualPost }: Props): React.ReactElement => {
+  // Fixes bug where image would not render on page load for some reason
+  const [shouldRenderImage, setShouldRenderImage] = useState(false);
+
+  useEffect(() => {
+    setShouldRenderImage(true);
+  }, []);
+
   return (
     <div className="relative h-[106px] w-[106px] bg-gray-200">
-      <Image
-        alt={actualPost.caption || `Instagram post thumbnail`}
-        fill
-        sizes="250px"
-        src={`/upload/deanna-troy-travels/instagram/${actualPost.id}`}
-        style={{ objectFit: `cover`, objectPosition: `center` }}
-      />
+      {shouldRenderImage && (
+        <Image
+          alt={actualPost.caption || `Instagram post thumbnail`}
+          fill
+          sizes="250px"
+          src={`/upload/deanna-troy-travels/instagram/${actualPost.id}`}
+          style={{ objectFit: `cover`, objectPosition: `center` }}
+        />
+      )}
     </div>
   );
 };

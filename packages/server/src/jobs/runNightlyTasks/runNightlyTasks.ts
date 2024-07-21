038ad3957b4102a5e6d942ef4ab94e5a4d8c2426
Cedric <mcdougal.ce@gmail.com>
2024-07-21 08:46:37 -0400

    Move refreshInstagramAccessAllUsers to its own cron job

--- packages/server/src/jobs/runNightlyTasks/runNightlyTasks.ts
+++ packages/server/src/jobs/runNightlyTasks/runNightlyTasks.ts
@@ -3,7 +3,6 @@ import { runJobs } from '@/server/jobsRunner';
 export default async (): Promise<void> => {
   await runJobs([
     { name: `refreshActualPostMediaUrlsAllUsers`, data: {} },
-    { name: `refreshInstagramAccessAllUsers`, data: {} },
     { name: `refreshPlannedPostMediaUrlsAllUsers`, data: {} },
     { name: `syncInstagramAllUsers`, data: {} },
   ]);

694d7f47d870c12294f90e3a0f8b634ec19150b9
Cedric <mcdougal.ce@gmail.com>
2024-07-21 14:52:27 -0400

    Fix broken migration state

--- packages/db/drizzle/0026_red_living_lightning.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "postplan"."actual_post" ADD COLUMN "media_thumbnail_url_expires_at" timestamp;--> statement-breakpoint
-ALTER TABLE "postplan"."planned_post_media_item" ADD COLUMN "media_thumbnail_url_expires_at" timestamp;--> statement-breakpoint
-ALTER TABLE "postplan"."planned_post_media_item" ADD COLUMN "media_url_expires_at" timestamp;
--- packages/db/drizzle/0028_woozy_krista_starr.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "postplan"."user" ALTER COLUMN "email" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "postplan"."user" DROP COLUMN IF EXISTS "name";
